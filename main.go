a114e26a19198d6040a8fbc1739b3a40059f0a8b
minchao <minchao.220@gmail.com>
2017-06-14 12:40:05 +0800

    Integrate mux and negroni in HTTP server

--- main.go
+++ main.go
@@ -4,24 +4,35 @@ import (
 	"flag"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
 )
 
 func main() {
 	var (
-		addr string
-		dist string = "./webapp/dist"
+		addr   string
+		dist   string = "./webapp/dist"
+		router *mux.Router
 	)
 
 	flag.StringVar(&addr, "addr", ":8080", "Address")
 	flag.Parse()
 
+	// Serving static files
 	dir := http.Dir(dist)
 	f, err := dir.Open("index.html")
 	if err != nil {
 		log.Fatalf("The '%s' directory not found", dist)
 	}
 	f.Close()
-	http.Handle("/", http.FileServer(dir))
+
+	router = mux.NewRouter().StrictSlash(true)
+	router.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(dir)))
+
+	n := negroni.New()
+	n.UseHandler(router)
+
 	log.Printf("Listening for HTTP on %s", addr)
-	log.Fatal(http.ListenAndServe(addr, nil))
+	log.Fatal(http.ListenAndServe(addr, n))
 }
